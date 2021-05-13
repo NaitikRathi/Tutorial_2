f2cf5e85f1c8f79be38d63cb81289a1ad54c9b5c
NaitikRathi <84074226+NaitikRathi@users.noreply.github.com>
2021-05-13 17:41:46 +0530

    Add files via upload

---
+++ Player.gd
@@ -0,0 +1,19 @@
+extends KinematicBody2D
+
+const ACCELERATION = 10
+const MAX_SPEED = 2
+const FRICTION = 10
+
+var velocity = Vector2.ZERO
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
+	if input_vector != Vector2.ZERO:
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	
+	move_and_collide(velocity)
